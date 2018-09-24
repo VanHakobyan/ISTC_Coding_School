c5c17d45ad6118d006d9993fdba2a99d1c6cead7
vanhakobyan <vanhakobyan1996@gmail.com>
2018-09-24 20:56:11 +0400

    add first app

---
+++ FirtStage/Types/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 15;
+            char sim = 'a';
+            string s = "name";
+            float k = 15.12f;
+            double d = 123;
+        }
+    }
+}
