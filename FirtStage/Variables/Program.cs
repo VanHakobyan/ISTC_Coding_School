6e4d2576173564f49683259470811717add6a475
vanhakobyan <vanhakobyan1996@gmail.com>
2018-09-28 20:51:11 +0400

    add people description concat. dynemic and var const

---
+++ FirtStage/Variables/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "John";
+            const string lastName = "Smith";
+            const byte age = 52;
+            const string gender = "male";
+
+            //bad practice 
+            string badText = firstName + '\n' + lastName + "\t" + age + " " + gender;
+
+            //good practice
+            string goodText = $"firstName : {firstName} \nlastName : {lastName} \nage : {age} \ngender : {gender}";
+            firstName = "Alex";
+
+            Console.WriteLine(goodText);
+            Console.WriteLine(firstName);
+
+            //var x = 5;
+            //x = "hello"; //error
+
+            //dynamic a = 45;
+            //a = "asc";
+
+            int b = 4;
+            if (b<=15)
+            {
+
+            }
+        }
+
+    }
+}
