ad70d42c8866df0d1b3be92c37bfefac6760687e
vanhakobyan <vanhakobyan1996@gmail.com>
2018-10-01 20:15:46 +0400

    add calculators

---
+++ FirtStage/Calculator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input operation");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("Please input number1 = ");
+            double number1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please input number2 = ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+            bool flag = true;
+
+            switch (operation)
+            {
+                case "+":
+                    result = number1 + number2;
+                    break;
+                case "-":
+                    result = number1 + number2;
+                    break;
+                case "*":
+                    result = number1 + number2;
+                    break;
+                case "/":
+                    result = number1 + number2;
+                    break;
+                default:
+                    flag = false;
+                    break;
+            }
+
+            string resultString = $"{number1} {operation} {number2} = {result}";
+
+            Console.WriteLine(flag ? resultString : "Wrong operation");
+
+
+
+
+
+            //if (operation == "+")
+            //    Console.WriteLine(Math.Abs(number1 + number2));
+            //else if (operation == "-")
+            //    Console.WriteLine(Math.Abs(number1 - number2));
+            //else if (operation == "*")
+            //    Console.WriteLine(Math.Abs(number1 * number2));
+            //else if (operation == "/")
+            //    Console.WriteLine(Math.Abs(number1 / number2));
+            //else
+            //    Console.WriteLine("error");
+        }
+    }
+}
---
+++ FirtStage/CalculatorGoodPractice/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorGoodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please input operation ");
+            string operation = Console.ReadLine();
+
+            Console.Write("Please input number1 = ");
+            double number1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Please input number2 = ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+            bool flag = true;
+
+            switch (operation)
+            {
+                case "+":
+                    result = number1 + number2;
+                    break;
+                case "-":
+                    result = number1 - number2;
+                    break;
+                case "*":
+                    result = number1 * number2;
+                    break;
+                case "/":
+                    result = number1 / number2;
+                    break;
+                default:
+                    flag = false;
+                    break;
+            }
+
+            Console.WriteLine(flag ? $"{number1}  {operation} {number2} = {result}" : "Wrong operation");
+        }
+    }
+}
