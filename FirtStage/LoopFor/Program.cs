88b04c8d1e14f7923bb093c0d19fad2c36192c81
vanhakobyan <vanhakobyan1996@gmail.com>
2018-10-05 20:51:01 +0400

    add loop for

---
+++ FirtStage/LoopFor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count*count; i+=2)
+            {
+                Console.WriteLine($"i={i}");
+                Console.WriteLine($"i*i={i * i}");
+                if(i==4) break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
