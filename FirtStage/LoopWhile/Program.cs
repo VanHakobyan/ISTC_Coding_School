824606586e30f68fedb8ec99a5847c45e942954e
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-10-08 20:00:48 +0400

    add simple while construc.

---
+++ FirtStage/LoopWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+            while (number >= 0)
+            {
+                number = int.Parse(Console.ReadLine());
+                Console.WriteLine("all ok");
+            }
+            Console.WriteLine("bad !!!");
+            Console.ReadKey();
+        }
+    }
+}
