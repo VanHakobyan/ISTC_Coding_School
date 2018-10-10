9f39a6b918be3c4566ac03ff38cb9ec3fbf11776
vanhakobyan <vanhakobyan1996@gmail.com>
2018-10-10 20:52:57 +0400

    add methods

---
+++ FirtStage/Methods/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Methotds2;
+
+namespace Methods
+{
+    class Program
+    {
+        static int Add(int number1, int number2)
+        {
+            int result = number2 + number1;
+
+            return result;
+        }
+
+        static long Add(long n2, long n1)
+        {
+            return n1+n2;
+        }
+       
+        static decimal Add(decimal n2,decimal n1)
+        {
+            return n2+n1;
+        }
+
+        static byte Add(byte n2, byte n1)
+        {
+            return (byte)(n2+n1);
+        }
+
+        static byte Add(byte n2, byte n1,string name)
+        {
+            Console.WriteLine(name);
+            return (byte)(n2 + n1);
+        }
+
+        static byte Add(byte n2, string str, string name)
+        {
+            Console.WriteLine(name);
+            return (byte)(n2);
+        }
+        
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ FirtStage/Methotds2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methotds2
+{
+    public class Program
+    {
+        
+        /// <summary>
+        /// Type 1
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static string ConvertToBinary(int number)
+        {
+            return Convert.ToString(number,2);
+        }
+
+        /// <summary>
+        /// Type 2
+        /// </summary>
+        /// <param name="text"></param>
+        static void PrintText(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        /// <summary>
+        /// Type 3
+        /// </summary>
+        /// <returns></returns>
+        static double GetPi()
+        {
+            return Math.PI;
+        }
+
+        /// <summary>
+        /// Type 4
+        /// </summary>
+        static void PrintIntMaxValue()
+        {
+            Console.WriteLine(int.MaxValue);
+        }
+
+        static void Delay()
+        {
+            Console.ReadKey();
+        }
+        
+
+        static void Main(string[] args)
+        {
+            //Type 1 
+            string convertToBinary = ConvertToBinary(8);
+            Console.WriteLine(convertToBinary);
+            
+            //Type 2
+            PrintText("Hello");
+
+            //Type 3
+            double pi = GetPi();
+            Console.WriteLine(pi);
+
+            //Type 4
+            PrintIntMaxValue();
+
+            //Type 4
+            Delay();
+        }
+    }
+}
