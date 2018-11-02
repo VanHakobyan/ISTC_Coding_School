4a50ac1a4ff71f2ee05f3859a2c3f60c7f4c03c2
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-11-03 00:42:23 +0400

    add encaps.

---
+++ SecondStage_OOP/Encapsulation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Person
+    {
+        static Person()
+        {
+
+        }
+
+        public Person()
+        {
+
+        }
+
+
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        //    public Person(string fn, string ln, int age)
+        //    {
+        //        FirstName = fn;
+        //        LastName = ln;
+        //        Age = age;
+        //    }
+
+        //public string GetName()
+        //{
+        //    return FirstName;
+        //}
+        //public void SetName(string fn)
+        //{
+        //    FirstName = fn;
+        //}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            Person person = new Person()
+            {
+                //FirstName = "fn",
+                LastName = "last",
+                Age = 87,
+            };
+
+
+
+        }
+    }
+}
