58a065831ff43488eabd0d5f3e49b32f782e75fc
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-11-09 20:56:25 +0400

    add 6 classWork

---
+++ SecondStage_OOP/ClassLibraryInternal/MyClass2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryInternal
+{
+
+    public class MyClass2
+    {
+        public class MyClass3
+        {
+
+        }
+        public int x;
+        private int y;
+        protected int z;
+        internal int k;
+        protected internal int pi;
+        MyClass3 myClass3 = new MyClass3();
+    }
+}
---
+++ SecondStage_OOP/Constructors3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryInternal;
+using Constructors3;
+using Console = System.Console;
+
+namespace Constructors3
+{
+    internal class MyClass
+    {
+        public static int X { get; set; }
+
+        protected internal MyClass(int x)
+        {
+
+        }
+
+        
+        static void Method()
+        {
+            Console.WriteLine("1651");
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass2 myClass = new MyClass2();
+        }
+    }
+}
+
+
