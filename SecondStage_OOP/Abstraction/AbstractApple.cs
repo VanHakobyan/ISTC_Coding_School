849395101aa50c018273b428e5a9f3200871bd61
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-11-13 00:05:33 +0400

    add abstraction

---
+++ SecondStage_OOP/Abstraction/AbstractApple.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+
+namespace Abstraction
+{
+    public abstract class AbstractClass //Base
+    {
+        public AbstractClass(string name)
+        {
+            _name = name;
+        }
+        private string _name;
+        //private abstract string Name; //Field error
+        public abstract int PI { get; set; }
+        public virtual int Prop { get; set; }
+
+        public abstract string Method();
+
+        public virtual int GetYear()
+        {
+            return DateTime.Now.Year;
+        }
+        public double GetE()
+        {
+            return Math.E;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+
+    public class ConcreteClass : AbstractClass
+    {
+        public ConcreteClass(string name) : base(name)
+        {
+
+        }
+
+        public override int PI
+        {
+            get
+            {
+                return (int)Math.PI;
+            }
+            set { }
+        }
+        public override string Method()
+        {
+            return "";
+        }
+    }
+    public abstract class AbstractApple
+    {
+        private string type;
+        private string color;
+
+        public AbstractApple(string type, string color)
+        {
+            this.type = type;
+            this.color = color;
+        }
+
+        public string GetAppleType()
+        {
+            return type;
+        }
+
+        public string GetColor()
+        {
+            return color;
+        }
+    }
+
+    public class GoldenApple : AbstractApple
+    {
+        public GoldenApple(string type, string color) : base(type, color)
+        {
+        }
+        public int MyProperty { get; set; }
+    }
+
+    public class DemirApple : AbstractApple
+    {
+        public DemirApple(string type, string color) : base(type, color)
+        {
+        }
+    }
+
+    public class NorTarvaSexaniApple : AbstractApple
+    {
+        public NorTarvaSexaniApple(string type, string color) : base(type, color)
+        {
+        }
+    }
+}
---
+++ SecondStage_OOP/Abstraction/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GoldenApple goldenApple = new GoldenApple("golden", "Yellow");
+
+            NorTarvaSexaniApple norTarvaSexaniApple = new NorTarvaSexaniApple("nor tarva", "red");
+            AbstractApple abstractApple = null; //base
+            //abstractApple = (AbstractApple)demirApple;
+            //norTarvaSexaniApple = (NorTarvaSexaniApple)abstractApple;
+
+            int number = 0;
+            Console.Write("Num: ");
+            number = int.Parse(Console.ReadLine());
+            switch (number)
+            {
+                case 1: abstractApple = goldenApple; break;
+                case 2: abstractApple = new DemirApple("demir", "green"); ; break;
+                case 3: abstractApple = norTarvaSexaniApple; break;
+                default: break;
+
+            }
+
+            Console.WriteLine($"Color {abstractApple.GetColor()}");
+            Console.WriteLine($"AppleType {abstractApple.GetAppleType()}");
+            GoldenApple goldenApple1 = (GoldenApple)abstractApple;
+            int x = goldenApple1.MyProperty;
+            int a = goldenApple.MyProperty;
+        }
+    }
+}
