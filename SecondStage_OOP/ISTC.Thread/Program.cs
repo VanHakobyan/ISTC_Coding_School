9168c1c6c8b010eb2c965c8e2fee99a6f6d313c4
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-11-23 21:04:58 +0400

    add threads and process

---
+++ SecondStage_OOP/ISTC.Thread/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ISTC.Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = Thread.CurrentThread;
+            Console.WriteLine(thread.IsAlive);
+            Console.WriteLine(thread.Name);
+            Console.WriteLine(thread.GetHashCode());
+            Process.Start("https://www.facebook.com/");
+
+
+        }
+    }
+}
--- SecondStage_OOP/Structures/Program.cs
+++ SecondStage_OOP/Structures/Program.cs
@@ -21,10 +21,65 @@ namespace Structures
         }
     }
 
+    class A
+    {
+        private int x;
+        private int y;
+
+        public A(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Plus()
+        {
+            x++;
+            y++;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+    struct B
+    {
+        public int x;
+        public int y;
+
+        public B(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Plus()
+        {
+            x++;
+            y++;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            A a = new A(10, 20);
+            a.Plus();
+            a.Print();
+
+            B b=new B(10,20);
+            b.Plus();
+            b.Print();
+
             MyStruct myStruct = new MyStruct(5);
             Console.WriteLine(myStruct.field);
             Console.ReadKey();
