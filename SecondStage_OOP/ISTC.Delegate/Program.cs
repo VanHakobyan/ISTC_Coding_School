0d22492932f813ff2437546491f9772f35c3855b
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-11-26 21:06:25 +0400

    Delegate

---
+++ SecondStage_OOP/ISTC.Delegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.Delegate
+{
+    class Program
+    {
+        public delegate string MyDelegate1(int x, string s);
+        public delegate string MyDelegate2(int x, string s);
+
+        public static string MyMethod1(int x, string s)
+        {
+            Console.WriteLine(x);
+            return s;
+        }
+        public static string MyMethod2(int x, string s)
+        {
+            Console.WriteLine(x + 1);
+            return s + "1";
+        }
+
+        static void Main(string[] args)
+        {
+
+            MyDelegate1 myDelegate1 = new MyDelegate1(MyMethod1);
+            MyDelegate1 myDelegate2 = new MyDelegate1(MyMethod2);
+            MyDelegate1 mydel = myDelegate1 + myDelegate2;
+            mydel.Invoke(5,"Han");
+
+        }
+    }
+}
---
+++ SecondStage_OOP/ISTC.UsingDelegates/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace ISTC.UsingDelegates
+{
+    class PrintStringClass
+    {
+        static FileStream fs; 
+        static StreamWriter sw; 
+
+        public delegate void PrintString(string s);
+
+
+        public static void WriteToScreen(string str)
+        {
+            Console.WriteLine($"The String is: {str}");
+        }
+
+
+        public static void WriteToFile(string s)
+        {
+            fs = new FileStream(@"C:\Users\vanik.hakobyan\source\message&amp;.txt", FileMode.Append, FileAccess.Write);
+            sw = new StreamWriter(fs);
+            sw.WriteLine(s);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+
+
+        public static bool sendString(PrintString ps)
+        {
+            ps.Invoke("Hello World");
+            return true;
+        }
+
+
+        class Program
+        {
+            unsafe static void FastCopy(byte[] src, byte[] dst, int count)
+            {
+                
+            }
+            static void Main(string[] args)
+            {
+               
+
+
+                PrintString ps1 = new PrintString(WriteToScreen);
+                PrintString ps2 = new PrintString(WriteToFile);
+                //ps1+=WriteToFile;
+
+                bool flag1 = sendString(ps1);
+                bool flag2 = sendString(ps2);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
