fb7f31830c577566f2f77c078932b1b3844ed768
vanik hakobyan <vanik.hakobyan@betconstruct.com>
2018-11-28 11:50:03 +0400

    Method  Operator  Expression

---
+++ SecondStage_OOP/ISTC.Lambada/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ISTC.Lambada
+{
+    public delegate double Calculate(float num);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lambada
+
+            Calculate calculate1 = delegate (float n) { return n * n; };     // Method
+            Calculate calculate2 = (n) => { return n * n; };                 // Operator
+            Calculate calculate3 = n => n * n;                               // Expression
+
+            //calculate1=calculate1=calculate1 
+        }
+    }
+}
