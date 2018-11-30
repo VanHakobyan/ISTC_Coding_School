af101a545166ae8725a1f477186ad2e777e2b02b
vanhakobyan <vanhakobyan1996@gmail.com>
2018-11-30 17:12:33 +0400

    many catch

---
+++ ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.ExceptionHandling3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.SecondStage.OOP.ExceptionHandling3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new NullReferenceException("Null exception trowed !!!");
+            }
+
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e) when (e is AggregateException || e is ContextMarshalException)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+        }
+    }
+}
