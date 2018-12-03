1efd173d8a5dfb3aced184b9e67febf84e734f3a
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-12-03 20:58:54 +0400

    add first null2

---
+++ ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.Nullable02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.SecondStage.OOP.Nullable02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? number = 5;
+            int? res = number.HasValue ? number : null;
+
+            string[] arr = new string[3];
+
+
+            if (arr != null && arr[2] != null)
+            {
+                int x = arr[2].Length;
+            }
+            int? y = arr?[2]?.Length.ToString()?.Length.ToString()?.Length;
+        }
+    }
+}
