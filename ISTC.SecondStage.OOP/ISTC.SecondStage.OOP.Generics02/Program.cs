c1740fab890ebce5a2fc4c3c4630ced1d9f04340
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-03 15:26:06 +0400

    add swap generic method

--- ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.Generics02/Program.cs
+++ ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.Generics02/Program.cs
@@ -8,8 +8,29 @@ namespace ISTC.SecondStage.OOP.Generics02
 {
     class Program
     {
+        static void Swap<T>(ref T first, ref T second)
+        {
+            T temp;
+            temp = first;
+            first = second;
+            second = temp;
+        }
         static void Main(string[] args)
         {
+            int a = 1;
+            int b = 2;
+            char c = 'c';
+            char d = 'd';
+
+
+            Swap(ref d, ref c);
+            Swap(ref a, ref b);
+
+
+            Console.WriteLine($"a={a}");
+            Console.WriteLine($"b={b}");
+            Console.WriteLine($"b={c}");
+            Console.WriteLine($"d={d}");
         }
     }
 }
