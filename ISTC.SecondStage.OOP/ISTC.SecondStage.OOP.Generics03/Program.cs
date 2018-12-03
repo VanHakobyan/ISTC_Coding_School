750b9dd62da54526ee3ce4b94437c99cc2b54fae
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-03 15:29:45 +0400

    add generic delegate

---
+++ ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.Generics03/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate T NumberChanger<T>(T n);
+namespace ISTC.SecondStage.OOP.Generics03
+{
+    class Program
+    {
+        static int num = 10;
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            //create delegate instances
+            NumberChanger<int> nc1 = new NumberChanger<int>(AddNum);
+            NumberChanger<int> nc2 = new NumberChanger<int>(MultNum);
+
+            //calling the methods using the delegate objects
+            nc1(25);
+            Console.WriteLine($"Value of Num: {getNum()}");
+
+            nc2(5);
+            Console.WriteLine($"Value of Num: {getNum()}");
+            Console.ReadKey();
+        }
+    }
+}
