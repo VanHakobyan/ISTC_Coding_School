07a6a9c948cd43dca64cdd0b13961042ed20f808
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-03 13:25:13 +0400

    add Generics 01

---
+++ ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.Generics01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.SecondStage.OOP.Generics01
+{
+    class MyGenericClass<T>
+    {
+        private T genericMemberVariable;
+
+        public MyGenericClass(T value)
+        {
+            genericMemberVariable = value;
+        }
+
+        public T GenericMethod(T genericParameter)
+        {
+            Console.WriteLine("Parameter type: {0}, value: {1}", typeof(T).ToString(), genericParameter);
+            Console.WriteLine("Return type: {0}, value: {1}", typeof(T).ToString(), genericMemberVariable);
+
+            return genericMemberVariable;
+        }
+
+        public T GenericProperty { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGenericClass<int> intGenericClass = new MyGenericClass<int>(10);
+
+            int val = intGenericClass.GenericMethod(1500);
+        }
+    }
+}
