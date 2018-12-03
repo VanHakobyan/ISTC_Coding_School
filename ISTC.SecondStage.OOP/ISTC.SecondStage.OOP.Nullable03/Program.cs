9b97ac344473844f74b9e85ddce29ff55f6252a7
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-12-03 20:59:01 +0400

    add first null3

---
+++ ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.Nullable03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.SecondStage.OOP.Nullable03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = null;
+            string result = s ?? string.Empty;//"" or null
+
+            int? k = null;
+            double? d = null;
+            k = 5;
+            //int k = 2;
+            //decimal d = .25m;
+
+            if (d == k)
+            {
+                Console.WriteLine("if");
+            }
+            else
+            {
+                Console.WriteLine("else");
+            }
+
+        }
+    }
+}
