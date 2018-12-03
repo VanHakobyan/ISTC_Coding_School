09e478b05f104a20c9f209db0a7f68730167b873
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-12-03 20:58:46 +0400

    add first null1

---
+++ ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.Nullable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.SecondStage.OOP.Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> x = null;// T?=Nullable<T> 
+            Nullable<int> y = 5;
+
+            if (x.HasValue)
+            {
+                x++;
+            }
+            else
+            {
+                x = y;
+                int t = x.Value;//null.Value;
+            }
+        }
+    }
+}
