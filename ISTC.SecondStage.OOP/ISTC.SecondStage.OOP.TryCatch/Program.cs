9f7f9846fdd75b67a30fc79b591e06c9d2ec3582
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-12-03 20:59:23 +0400

    minor ex

---
+++ ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.TryCatch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.SecondStage.OOP.TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char? firstNumber = null;
+            try
+            {
+                firstNumber = char.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.GetType());
+            }
+            Console.WriteLine(firstNumber);
+        }
+    }
+}
