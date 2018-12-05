9ca01922cebd98f3bf22224d3990bd6292f7eb87
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-05 16:37:21 +0400

    add LINQ 02

---
+++ ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.LINQ02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.SecondStage.OOP.LINQ02
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] studentList = new Student[5] {
+                new Student { StudentId = 1, StudentName = "John", Age = 18 } ,
+                new Student { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                new Student { StudentId = 3, StudentName = "Bill",  Age = 18 } ,
+                new Student { StudentId = 4, StudentName = "Ram" , Age = 20  } ,
+                new Student { StudentId = 5, StudentName = "Ron" , Age = 21 }
+            };
+
+            var studentNames = from s in studentList
+                               select new
+                               {
+                                   StudentID = s.StudentId,
+                                   StudentName = s.StudentName
+                               };
+
+            for (int i = 0; i < studentList.Length; i++)
+            {
+                Console.Write(studentList[i].StudentId + ".");
+                Console.WriteLine(studentList[i].StudentName);
+                Console.WriteLine(new string('-', 50));
+            }
+        }
+    }
+}
