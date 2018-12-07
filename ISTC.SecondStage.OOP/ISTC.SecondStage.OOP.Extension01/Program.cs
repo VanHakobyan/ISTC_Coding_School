7f2b932a24a5556feb457041fceeb177ef020ea8
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-07 16:06:55 +0400

    add extension 01

---
+++ ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.Extension01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.SecondStage.OOP.Extension01
+{
+    public static class IntExtensions
+    {
+        public static bool IsGreaterThan(this int i, int value) //this  - syntax sugar 
+        {
+            return i > value;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int element = 5;
+            var isGreaterThan = element.IsGreaterThan(100);
+            Console.WriteLine($"isGreaterThan? - {isGreaterThan}");
+        }
+    }
+}
