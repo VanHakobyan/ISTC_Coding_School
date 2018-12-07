d2b318d12bed5ecad4d28f5010450aada5f8f2da
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-07 15:34:07 +0400

    add operator proj

---
+++ ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.Operator01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.SecondStage.OOP.Operator01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
