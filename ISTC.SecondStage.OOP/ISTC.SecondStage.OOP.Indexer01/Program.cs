cacbe65b3dfa7af953f5559d2752fc661a663611
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-07 16:46:26 +0400

    add indexer

---
+++ ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.Indexer01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.SecondStage.OOP.Indexer01
+{
+    class StringDataStore
+    {
+
+        private string[] strArr = new string[10]; // internal data storage
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index < 0 && index >= strArr.Length)
+                    throw new IndexOutOfRangeException("Cannot store more than 10 objects");
+
+                return strArr[index];
+            }
+
+            set
+            {
+                if (index < 0 || index >= strArr.Length)
+                    throw new IndexOutOfRangeException("Cannot store more than 10 objects");
+
+                strArr[index] = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringDataStore strStore = new StringDataStore();
+
+            strStore[0] = "One";
+            strStore[1] = "Two";
+            strStore[2] = "Three";
+            strStore[3] = "Four";
+
+            //strStore[13] = "Thirteen";//exception 
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(strStore[i]);
+            }
+        }
+    }
+}
