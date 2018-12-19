be0a762eb9d6c33ff0b67636254f1a152fc06f0d
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-19 15:57:29 +0400

    Dir

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.File.Directory/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.File.Directory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dirinfo = new DirectoryInfo( @"C:\\Windows");
+            Console.WriteLine($"Full Name of the directory is : {dirinfo.FullName}");
+            Console.WriteLine($"The directory was last accesses on: { dirinfo.LastAccessTime}");
+            Console.ReadKey();
+        }
+    }
+}
