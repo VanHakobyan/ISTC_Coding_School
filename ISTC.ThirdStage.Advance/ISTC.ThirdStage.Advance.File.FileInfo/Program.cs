b87b42533cb155808606ab7592c37a2b43d8eb42
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-19 15:57:40 +0400

    FileInfo

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.File.FileInfo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.File.FileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dirinfo = new DirectoryInfo("C:\\Windows");
+            System.IO.FileInfo[] filesInDir = dirinfo.GetFiles();
+            foreach (System.IO.FileInfo file in filesInDir)
+            {
+                Console.WriteLine($"File Name {file.FullName} Size: {file.Length} bytes");
+            }
+            Console.Read();
+        }
+    }
+}
