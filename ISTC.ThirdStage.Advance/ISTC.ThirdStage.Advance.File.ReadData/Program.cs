47e0d642f566059deb51a1499e07dc5c4667111b
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-19 15:57:46 +0400

    read

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.File.ReadData/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.File.ReadData
+{
+    class Program
+    {
+        public void ReadData()
+        {
+            FileStream fs = new FileStream("E:\\data.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string str = sr.ReadLine();
+            while (str != null)
+            {
+                Console.WriteLine(str);
+                str = sr.ReadLine();
+            }
+            sr.Close();
+            fs.Close();
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.ReadData();
+            Console.Read();
+        }
+    }
+}
