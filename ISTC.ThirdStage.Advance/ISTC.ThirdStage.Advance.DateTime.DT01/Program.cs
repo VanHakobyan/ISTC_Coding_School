378aa23d59036a8d2f9d00626f87df75bdbf99fa
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-19 16:33:40 +0400

    add dt1

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.DateTime.DT01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.DateTime.DT01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.DateTime myDate = System.DateTime.Now;
+            int year = myDate.Year;
+            int month = myDate.Month; 
+            int day = myDate.Day;
+            int hour = myDate.Hour; 
+            int minute = myDate.Minute; 
+            int second = myDate.Second; 
+            DayOfWeek weekDay = myDate.DayOfWeek; 
+        }
+    }
+}
