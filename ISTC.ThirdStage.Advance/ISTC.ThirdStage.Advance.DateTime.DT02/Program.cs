e5b97d6b4ceffa3a07969d1683108b36421d9a9e
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-19 16:33:44 +0400

    add dt1

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.DateTime.DT02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.DateTime.DT02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Current culture name  
+            string currentCulture = Thread.CurrentThread.CurrentCulture.DisplayName;
+
+            System.DateTime currentTime = System.DateTime.Now; //"1/9/2016 10:22:45 AM"  
+            //// Getting DateTime based on culture.  
+            string dateInUSA = currentTime.ToString("D", new CultureInfo("en-US")); // USA - Saturday, January 09, 2016  
+            string dateInHindi = currentTime.ToString("D", new CultureInfo("hi-IN")); // Hindi - 09 ????? 2016  
+            string dateInJapan = currentTime.ToString("D", new CultureInfo("ja-JP")); // Japan - 2016?1?9?  
+            string dateInFrench = currentTime.ToString("D", new CultureInfo("fr-FR")); // French - samedi 9 janvier 2016  
+
+            string dateInOriya = currentTime.ToString("D", new CultureInfo("or")); // Oriya - 09 ???????? 2016  
+            // Convert Hindi Date to French Date  
+            System.DateTime originalResult = new System.DateTime(2016, 01, 09);
+            // Converting Hindi Date to DateTime object  
+            bool success = System.DateTime.TryParse(dateInHindi, new System.Globalization.CultureInfo("hi-IN"),
+                System.Globalization.DateTimeStyles.None, out originalResult);
+
+            // Next convert current date to French date  
+            string frenchTDate = originalResult.ToString("D", new CultureInfo("fr-FR")); // French - samedi 9 janvier 2016  
+
+            // To get DatePattern from Culture  
+            CultureInfo fr = new CultureInfo("fr-FR");
+            string shortUsDateFormatString = fr.DateTimeFormat.LongDatePattern;
+            string shortUsTimeFormatString = fr.DateTimeFormat.ShortTimePattern;
+
+            // To get all installed culture in .Net version  
+            foreach (CultureInfo ci in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                Console.WriteLine(ci.Name + ", " + ci.EnglishName);
+            }
+
+        }
+    }
+}
