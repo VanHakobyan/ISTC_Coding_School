fc442f7d557b4ef505742ea092f2f42f43fd3e00
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-19 15:57:50 +0400

    Write

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.File.WriteData/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.File.WriteData
+{
+    class Program
+    {
+        public void WriteData()
+        {
+            FileStream fs = new FileStream("E://data.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter w = new StreamWriter(fs);
+            Console.WriteLine("Enter the string");
+        
+            string str = Console.ReadLine();
+            w.Write(str);
+            w.Flush();
+            fs.Close();
+
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.WriteData();
+            Console.Read();
+        }
+    }
+}
