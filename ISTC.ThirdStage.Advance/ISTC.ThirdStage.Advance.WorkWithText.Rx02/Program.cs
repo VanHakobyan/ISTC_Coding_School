5284c432e5bf8ea1b07b7ff499421ab242440004
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-21 12:11:30 +0400

    WWT Regex02

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.WorkWithText.Rx02/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.WorkWithText.Rx02
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string strToTest;
+          
+            Console.Write("Enter a string to Test for Alphabets:");
+            strToTest = Console.ReadLine();
+            if (IsAlpha(strToTest))
+            {
+                Console.WriteLine("{0} is Valid Alpha string", strToTest);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Valid Alpha string", strToTest);
+            }
+        }
+        // Function to test for Positive Integers.   
+        public static bool IsNaturalNumber(string strNumber)
+        {
+            Regex objNotNaturalPattern = new Regex("[^0-9]");
+            Regex objNaturalPattern = new Regex("0*[1-9][0-9]*");
+            return !objNotNaturalPattern.IsMatch(strNumber) &&
+            objNaturalPattern.IsMatch(strNumber);
+        }
+        // Function to test for Positive Integers with zero inclusive   
+        public static bool IsWholeNumber(string strNumber)
+        {
+            Regex objNotWholePattern = new Regex("[^0-9]");
+            return !objNotWholePattern.IsMatch(strNumber);
+        }
+        // Function to Test for Integers both Positive & Negative   
+        public static bool IsInteger(string strNumber)
+        {
+            Regex objNotIntPattern = new Regex("[^0-9-]");
+            Regex objIntPattern = new Regex("^-[0-9]+$|^[0-9]+$");
+            return !objNotIntPattern.IsMatch(strNumber) && objIntPattern.IsMatch(strNumber);
+        }
+        // Function to Test for Positive Number both Integer & Real   
+        public static bool IsPositiveNumber(string strNumber)
+        {
+            Regex objNotPositivePattern = new Regex("[^0-9.]");
+            Regex objPositivePattern = new Regex("^[.][0-9]+$|[0-9]*[.]*[0-9]+$");
+            Regex objTwoDotPattern = new Regex("[0-9]*[.][0-9]*[.][0-9]*");
+            return !objNotPositivePattern.IsMatch(strNumber) &&
+            objPositivePattern.IsMatch(strNumber) &&
+            !objTwoDotPattern.IsMatch(strNumber);
+        }
+        // Function to test whether the string is valid number or not  
+        public static bool IsNumber(string strNumber)
+        {
+            Regex objNotNumberPattern = new Regex("[^0-9.-]");
+            Regex objTwoDotPattern = new Regex("[0-9]*[.][0-9]*[.][0-9]*");
+            Regex objTwoMinusPattern = new Regex("[0-9]*[-][0-9]*[-][0-9]*");
+            string strValidRealPattern = "^([-]|[.]|[-.]|[0-9])[0-9]*[.]*[0-9]+$";
+            string strValidIntegerPattern = "^([-]|[0-9])[0-9]*$";
+            Regex objNumberPattern = new Regex("(" + strValidRealPattern + ")|(" + strValidIntegerPattern + ")");
+            return !objNotNumberPattern.IsMatch(strNumber) &&
+            !objTwoDotPattern.IsMatch(strNumber) &&
+            !objTwoMinusPattern.IsMatch(strNumber) &&
+            objNumberPattern.IsMatch(strNumber);
+        }
+        // Function To test for Alphabets.   
+        public static bool IsAlpha(string strToCheck)
+        {
+            Regex objAlphaPattern = new Regex("[^a-zA-Z]");
+            return !objAlphaPattern.IsMatch(strToCheck);
+        }
+        // Function to Check for AlphaNumeric.  
+        public static bool IsAlphaNumeric(string strToCheck)
+        {
+            Regex objAlphaNumericPattern = new Regex("[^a-zA-Z0-9]");
+            return !objAlphaNumericPattern.IsMatch(strToCheck);
+        }
+    }
+}
