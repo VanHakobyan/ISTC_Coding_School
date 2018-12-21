a9439c02dab75054fd8d9d749d4ddfd9af5380c9
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-12-22 00:42:22 +0400

    add webClient

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.WorkWithText.GetContent/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.WorkWithText.GetContent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client=new WebClient();
+            string content = client.DownloadString(@"https://github.com/vanhakobyan");
+            File.AppendAllText(@"E:\content.html",content);
+        }
+    }
+}
