7f7c944346cc07a687cb24fb593933a3c973787b
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-12-22 00:42:36 +0400

    Agility pack

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.WorkWithText.HTMLAP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace ISTC.ThirdStage.Advance.WorkWithText.HTMLAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlDocument document = new HtmlDocument();
+            string text = File.ReadAllText(@"E:\ISTC_Coding_School.html");
+            //Console.WriteLine(text);
+            document.LoadHtml(text);
+
+            HtmlNode selectSingleNode = document.DocumentNode.SelectSingleNode("//*[@id=\"js-repo-pjax-container\"]/div[1]/div/h1/span[1]/a");
+            string innerText = selectSingleNode.InnerText;
+          
+            Console.WriteLine(innerText);
+            Console.ReadKey();
+        }
+    }
+}
