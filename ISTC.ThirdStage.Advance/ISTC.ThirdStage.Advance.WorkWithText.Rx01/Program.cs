e6bc003b64025eecdbd2d0c24f7df9174f73bb19
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-21 12:11:12 +0400

    WWT regex01

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.WorkWithText.Rx01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.WorkWithText.Rx01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "4 AND 5";
+
+            // Get first match.
+            Match match = Regex.Match(value, @"\d");
+            if (match.Success)
+            {
+                Console.WriteLine(match.Value);
+            }
+
+            // Get second match.
+            match = match.NextMatch();
+            if (match.Success)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
