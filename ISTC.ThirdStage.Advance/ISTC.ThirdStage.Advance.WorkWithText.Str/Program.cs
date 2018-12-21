583f0e5e67f61f8dbeea270482b41e5c5c86e048
vanhakobyan <vanhakobyan1996@gmail.com>
2018-12-21 12:11:02 +0400

    WWTs

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.WorkWithText.Str/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.WorkWithText.Str
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "there is a cat";
+            // Split string on spaces (this will separate all the words).
+            string[] words = data.Split(' ');
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
