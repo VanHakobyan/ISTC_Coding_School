21aceb17eba628a47b0d709293115d1d157a46e5
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-12-26 18:58:43 +0400

    init configs

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.Configs.Json/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.Configs.Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
