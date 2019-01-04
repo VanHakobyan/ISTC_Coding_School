43b6690acf3bd78ce6866b77a2e3333677951f60
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2019-01-04 23:54:22 +0400

    add r1

--- ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.ReflectionAttributes.R1/Program.cs
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.ReflectionAttributes.R1/Program.cs
@@ -1,21 +1,53 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ISTC.ThirdStage.Advance.ReflectionAttributes.R1
 {
+    static class ReflectionTest
+    {
+        public static int Height;
+        public static int Width;
+        public static int Weight;
+        public static string Name;
+
+        public static void Write()
+        {
+            Type type = typeof(ReflectionTest);    // Get type pointer
+            FieldInfo[] fields = type.GetFields(); // Obtain all fields
+            foreach (var field in fields)          // Loop through fields
+            {
+                string name = field.Name;               // Get string name
+                object temp = field.GetValue(null); // Get value
+                if (temp is int)                        // See if it is an integer.
+                {
+                    int value = (int)temp;
+                    Console.Write(name);
+                    Console.Write(" (int) = ");
+                    Console.WriteLine(value);
+                }
+                else if (temp is string)                // See if it is a string.
+                {
+                    string value = temp as string;
+                    Console.Write(name);
+                    Console.Write(" (string) = ");
+                    Console.WriteLine(value);
+                }
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            ReflectionTest.Height = 100;    // Set value
+            ReflectionTest.Width = 50;      // Set value
+            ReflectionTest.Weight = 300;    // Set value
+            ReflectionTest.Name = "Perl";   // Set value
+            ReflectionTest.Write();         // Invoke reflection methods
         }
     }
 }
