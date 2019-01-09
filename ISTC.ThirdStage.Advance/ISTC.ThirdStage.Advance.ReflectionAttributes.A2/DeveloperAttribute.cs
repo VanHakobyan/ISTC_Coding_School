fa75b0759af6b0c9dc0c067c74d55d32e2821c32
vanhakobyan <vanhakobyan1996@gmail.com>
2019-01-09 15:24:29 +0400

    add custom attr

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.ReflectionAttributes.A2/DeveloperAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.ReflectionAttributes.A2
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public sealed class DeveloperAttribute : Attribute
+    {
+        // Private fields.
+
+        // This constructor defines two required parameters: name and level.
+
+        public DeveloperAttribute(string name, string level)
+        {
+            this.Name = name;
+            this.Level = level;
+            this.Reviewed = false;
+        }
+
+        // Define Name property.
+        // This is a read-only attribute.
+
+        public string Name { get; }
+
+        // Define Level property.
+        // This is a read-only attribute.
+
+        public string Level { get; }
+
+        // Define Reviewed property.
+        // This is a read/write attribute.
+
+        public bool Reviewed { get; set; }
+    }
+}
---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.ReflectionAttributes.A2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.ReflectionAttributes.A2
+{
+    [Developer("Joan Smith", "42", Reviewed = true)]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call function to get and display the attribute.
+            GetAttribute(typeof(Program));
+        }
+        public static void GetAttribute(Type t)
+        {
+            // Get instance of the attribute.
+            DeveloperAttribute MyAttribute = (DeveloperAttribute)Attribute.GetCustomAttribute(t, typeof(DeveloperAttribute));
+
+            if (MyAttribute == null)
+            {
+                Console.WriteLine("The attribute was not found.");
+            }
+            else
+            {
+                // Get the Name value.
+                Console.WriteLine("The Name Attribute is: {0}.", MyAttribute.Name);
+                // Get the Level value.
+                Console.WriteLine("The Level Attribute is: {0}.", MyAttribute.Level);
+                // Get the Reviewed value.
+                Console.WriteLine("The Reviewed Attribute is: {0}.", MyAttribute.Reviewed);
+            }
+        }
+    }
+}
