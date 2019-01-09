79a5cf3c5e83fe011b096fbb1f51fe0c5ce8d36e
vanhakobyan <vanhakobyan1996@gmail.com>
2019-01-09 14:40:14 +0400

    add simple DLL

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.ReflectionAttributes.DLL/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ISTC.ThirdStage.Advance.ReflectionAttributes.DLL
+{
+    public class Calculator
+    {
+        public Calculator() { }
+        private double _number;
+        public double Number { get; set; }
+        public void Clear() { _number = 0; }
+        private void DoClear() { Number = 0; }
+        public double Add(double number) { _number = number; return number; }
+        public static double Pi => Math.PI;
+        public static double GetPi() {return Pi;}
+    }
+}
+
