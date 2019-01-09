c49b5850662f4e9f822bbc9cd5acb72e3d14195d
vanhakobyan <vanhakobyan1996@gmail.com>
2019-01-09 14:56:25 +0400

    add attribute 01

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.ReflectionAttributes.A1/Person.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace ISTC.ThirdStage.Advance.ReflectionAttributes.A1
+{
+    public class Person
+    {
+        [JsonProperty("Անուն")]
+        public string FirstName { get; set; }
+        [JsonProperty("Ազգանուն")]
+        public string LastName { get; set; }
+        [JsonProperty("Տարիք")]
+        public int Age { get; set; }
+        [JsonIgnore]
+        public int PhoneNumber { get; set; }
+    }
+}
---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.ReflectionAttributes.A1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ISTC.ThirdStage.Advance.ReflectionAttributes.A1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person { FirstName = "Նառա", LastName = "Նառա դու սուո", Age = 45, PhoneNumber = 101020 };
+            var serializeObject = JsonConvert.SerializeObject(person,Formatting.Indented);
+            Console.WriteLine(serializeObject);
+            Console.ReadKey();
+        }
+    }
+}
