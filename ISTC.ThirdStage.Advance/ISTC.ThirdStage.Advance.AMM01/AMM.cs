76d222aaa5e7d6413f9de61dabffbbfef2a789ef
vanhakobyan <vanhakobyan1996@gmail.com>
2019-01-10 18:36:05 +0400

    add AMM and GC

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.AMM01/AMM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.AMM01
+{
+    class A
+    {
+        ~A()
+        {
+            Console.WriteLine("Destruct instance of A");
+        }
+        public void F()
+        {
+            Console.WriteLine("A.F");
+            Program.RefA = this;
+        }
+    }
+    class B
+    {
+        public A Ref;
+        ~B()
+        {
+            Console.WriteLine("Destruct instance of B");
+            Ref.F();
+        }
+    }
+}
---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.AMM01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.AMM01
+{
+    class Program
+    {
+        public static A RefA;
+        public static B RefB;
+
+        static void Main()
+        {
+            RefB = new B();
+            RefA = new A();
+            RefB.Ref = RefA;
+            RefB = null;
+            RefA = null;
+
+            // A and B now eligible for destruction
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            // B now eligible for collection, but A is not
+            if (RefA != null)
+                Console.WriteLine("RefA is not null");
+        }
+    }
+}
