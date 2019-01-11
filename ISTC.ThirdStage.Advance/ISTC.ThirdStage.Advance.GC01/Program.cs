813e41a204778adcd59dc5e471cdc8113bb48e77
vanhakobyan <vanhakobyan1996@gmail.com>
2019-01-11 16:36:25 +0400

    add simple Fin using

--- ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.GC01/Program.cs
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.GC01/Program.cs
@@ -10,6 +10,31 @@ namespace ISTC.ThirdStage.Advance.GC01
     {
         static void Main(string[] args)
         {
+            List<TestGarbageCollector>  testGarbageCollectors=new List<TestGarbageCollector>();
+            int i = 0;
+            for(;;i++)
+            {
+                try
+                {
+                    testGarbageCollectors.Add(new TestGarbageCollector());
+                }
+                catch (OutOfMemoryException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(new string('-',50));
+                    Console.WriteLine($"Created {i} items");
+                    Console.WriteLine(new string('-',50));
+                    Console.WriteLine(string.Join("\n", testGarbageCollectors.Select(x => x.GetHashCode())));
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    break;
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.GC01/TestGarbageCollector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.GC01
+{
+    public class TestGarbageCollector
+    {
+        private long[] array = new long[1024 * 1024 * 5];
+
+        public string GetArrayInfo()
+        {
+            return string.Join(",", array.Select(x => x.GetHashCode()));
+        }
+        ~TestGarbageCollector()
+        {
+            Console.WriteLine($"Object removed code is {GetHashCode()}");
+            Thread.Sleep(100);
+        }
+    }
+}
