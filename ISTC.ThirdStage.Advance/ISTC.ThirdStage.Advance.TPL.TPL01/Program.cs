0207c20299403e027af81fbeff99f12d4a107da0
vanhakobyan <vanhakobyan1996@gmail.com>
2019-01-14 17:20:32 +0400

    add TPL 01

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.TPL.TPL01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+
+namespace ISTC.ThirdStage.Advance.TPL.TPL01
+{
+    //Block 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action handler = new Action(Display);
+            handler.Invoke();
+
+            Console.WriteLine("Continue: Main");
+            
+            Console.ReadLine();
+        }
+
+        static void Display()
+        {
+            Console.WriteLine("Start: Display....");
+
+            int result = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                result += i * i;
+            }
+            Thread.Sleep(3000);
+            Console.WriteLine($"Result: {result}");
+            Console.WriteLine("Stop: Display....");
+        }
+    }
+}
+
