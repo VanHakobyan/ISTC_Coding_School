691c93cc14addf14f4b3754dca9cafbd4f47c259
vanhakobyan <vanhakobyan1996@gmail.com>
2019-01-16 10:43:28 +0400

    add AA 02

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.AsyncAwait.AA_02/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.AsyncAwait.AA_02
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // Start the HandleFile method.
+            Task<int> task = HandleFileAsync();
+
+            // Control returns here before HandleFileAsync returns.
+            // ... Prompt the user.
+            Console.WriteLine("Please wait patiently " +
+                              "while I do something important.");
+
+            // Do something at the same time as the file is being read.
+            string line = Console.ReadLine();
+            Console.WriteLine("You entered (asynchronous logic): " + line);
+
+            // Wait for the HandleFile task to complete.
+            // ... Display its results.
+            task.Wait();
+            var x = task.Result;
+            Console.WriteLine("Count: " + x);
+
+            Console.WriteLine("[DONE]");
+            Console.ReadLine();
+        }
+
+        static async Task<int> HandleFileAsync()
+        {
+            string file = @"C:\Logs\file.txt";
+            Console.WriteLine("HandleFile enter");
+            int count = 0;
+
+            // Read in the specified file.
+            // ... Use async StreamReader method.
+            using (StreamReader reader = new StreamReader(file))
+            {
+                string v = await reader.ReadToEndAsync();
+
+                // ... Process the file data somehow.
+                count += v.Length;
+
+                // ... A slow-running computation.
+                //     Dummy code.
+                for (int i = 0; i < 10000; i++)
+                {
+                    int x = v.GetHashCode();
+                    if (x == 0)
+                    {
+                        count--;
+                    }
+                }
+            }
+            Console.WriteLine("HandleFile exit");
+            return count;
+        }
+    }
+}
