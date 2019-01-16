6aa9b1e1da56f2daaa190d1b70cd229e525f5f1d
vanhakobyan <vanhakobyan1996@gmail.com>
2019-01-16 10:19:10 +0400

    add pl 02

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.Parallel.Pl_02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ISTC.ThirdStage.Advance.Parallel.Pl_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using For Loop \n");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("i = {0}, thread = {1}",
+                    i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
+
+            Console.WriteLine("\nUsing Parallel.For \n");
+
+            System.Threading.Tasks.Parallel.For(0, 10, i =>
+            {
+                Console.WriteLine("i = {0}, thread = {1}", i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
