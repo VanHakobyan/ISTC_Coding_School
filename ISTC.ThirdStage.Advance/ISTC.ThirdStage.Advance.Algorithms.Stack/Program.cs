c56af250cc3930e3ebe54d49601c42ea58f812fd
vanhakobyan <vanhakobyan1996@gmail.com>
2019-01-18 13:20:55 +0400

    add implementation stack

---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.Algorithms.Stack/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.ThirdStage.Advance.Algorithms.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackCustom<int> stack = new StackCustom<int>(100);
+            var random = new Random();
+
+            stack.Push(int.MaxValue);
+
+            for (int i = 0; i < 150; i++)
+            {
+                stack.Push(random.Next(10, 100));
+            }
+
+            Console.WriteLine("Press any key to pop all items");
+            Console.ReadKey();
+            for (int i = 0; i < 150; i++)
+            {
+                var item = stack.Pop();
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.Algorithms.Stack/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Permissions;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Collections;
+
+namespace ISTC.ThirdStage.Advance.Algorithms.Stack
+{
+    public class StackCustom<T>
+    {
+        private T[] s;
+        private int currentStackIndex;
+
+        public StackCustom(int N)
+        {
+            if (N < 0)
+                throw new ArgumentOutOfRangeException(N.ToString());
+
+            s = new T[N];
+            currentStackIndex = 0;
+        }
+
+        public void Push(T x)
+        {
+            if (currentStackIndex + 1 >= s.Length)
+            {
+                Array.Resize(ref s, (s.Length + 1) * 2);
+            }
+
+            s[currentStackIndex++] = x;
+        }
+
+        public T Pop()
+        {
+            if (currentStackIndex == 0)
+                throw new InvalidOperationException("The stack is empty");
+
+            T value = s[--currentStackIndex];
+            s[currentStackIndex] = default(T);
+            return value;
+        }
+    }
+}
