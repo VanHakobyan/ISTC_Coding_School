8413b92b4c694aaf5685c37e1b89aa5cae011a22
vanhakobyan <vanhakobyan1996@gmail.com>
2019-01-25 16:47:04 +0400

    add git api

--- ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.Finally.LastProject/Program.cs
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.Finally.LastProject/Program.cs
@@ -8,8 +8,10 @@ namespace ISTC.ThirdStage.Advance.Finally.LastProject
 {
     class Program
     {
+        private const string GithubAllArmenianUserUrl = @"https://api.github.com/search/users?q=location:armenia&page={pageNumber}&per_page=100";
         static void Main(string[] args)
         {
+
         }
     }
 }
