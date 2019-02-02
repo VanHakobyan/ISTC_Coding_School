4a6a7ebfbd0b2b0f0083eb4fb9988cb626137925
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2019-02-02 16:09:49 +0400

    add DDL create

--- ISTC.FourthStage.Database/Data Definition Language/DDL_Create.sql
+++ ISTC.FourthStage.Database/Data Definition Language/DDL_Create.sql
@@ -1,1 +1,16 @@
+CREATE DATABASE MyFirstDataBase
+
+use MyFirstDataBase
+
+create table People 
+(
+	Id int identity,
+	FirstName nvarchar(250) not null,
+	LastName nvarchar(250) not null,
+	Age int null,
+	Country varchar(150),
+	City varchar(150),
+	BirthDate datetime null,
+	IsMarried bit not null
+)
 
