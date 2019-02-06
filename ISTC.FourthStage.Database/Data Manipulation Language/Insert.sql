a87b46a74adbbf115b8bcdbf1bde84d7448111b9
vanhakobyan <vanhakobyan1996@gmail.com>
2019-02-06 11:52:54 +0400

    add insert

---
+++ ISTC.FourthStage.Database/Data Manipulation Language/Insert.sql
@@ -0,0 +1,12 @@
+insert into People (Firstname,lastname,IsMarried)
+values
+('John','Smith',0),
+('Jimmy','Honlon',1)
+
+
+insert into People 
+values
+('Filipe','Coutinho',28,'Brasil','Rio de Janeiro',null,0)
+
+
+
