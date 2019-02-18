8e6be2763f6809119a240d54b2e8cff6d8f53d4a
vanhakobyan <vanhakobyan1996@gmail.com>
2019-02-18 10:48:21 +0400

    have added code first

---
+++ ISTC.FourthStage.Database/ISTC.FourthStage.Database.EF.CodeFirst/MyContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace ISTC.FourthStage.Database.EF.CodeFirst
+{
+    class MyContext : DbContext
+    {
+        public MyContext()
+            : base("name=MyContext")
+        {
+        }
+
+
+        public virtual DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ ISTC.FourthStage.Database/ISTC.FourthStage.Database.EF.CodeFirst/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.FourthStage.Database.EF.CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyContext context = new MyContext();
+            context.Persons.Add(new Person { FirstName = "Luke", LastName = "Daniels", Age = 21 });
+            context.SaveChanges();
+            var persons = context.Persons.ToList();
+            foreach (var person in persons)
+            {
+                Console.WriteLine($"{person.Id}:{person.FirstName} {person.LastName} {person.Age}");
+            }
+        }
+    }
+}
