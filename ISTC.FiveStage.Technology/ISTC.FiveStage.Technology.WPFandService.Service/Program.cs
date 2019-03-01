80679feaf7ee109484bc57ece5fd2945de7ae4e0
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2019-03-01 19:49:21 +0400

    added first service

---
+++ ISTC.FiveStage.Technology/ISTC.FiveStage.Technology.WPFandService.Service/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.FiveStage.Technology.WPFandService.Service
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            Service1 service = new Service1();
+            service.TestAndStart();
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1(), 
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+    }
+}
---
+++ ISTC.FiveStage.Technology/ISTC.FiveStage.Technology.WPFandService.Service/Service1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+
+namespace ISTC.FiveStage.Technology.WPFandService.Service
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            using (FileStream fileStream = new FileStream("E:\\Text1.txt", FileMode.OpenOrCreate))
+            {
+                using (StreamWriter streamWriter = new StreamWriter(fileStream))
+                {
+                    streamWriter.Write("Hello service!!!");
+                }
+            }
+
+        }
+
+        protected override void OnStop()
+        {
+            if (File.Exists("E:\\Text1.txt")) File.Delete("E:\\Text1.txt");
+        }
+
+
+        public void TestAndStart()
+        {
+            OnStart(null);
+            Console.ReadKey();
+            OnStop();
+        }
+    }
+}
