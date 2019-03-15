37736396275988977a7720ca928c39ccf8422cf9
vanhakobyan <vanhakobyan1996@gmail.com>
2019-03-15 17:25:17 +0400

    add mid

---
+++ ISTC.FiveStage.Technology/ISTC.FiveStage.Technology.Middleware_01/Controller/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ISTC.FiveStage.Technology.Middleware_01.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET: api/Home
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Home/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Home
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Home/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ISTC.FiveStage.Technology/ISTC.FiveStage.Technology.Middleware_01/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ISTC.FiveStage.Technology.Middleware_01
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine("A (before)");
+                await next();
+                Console.WriteLine("A (after)");
+            });
+
+            app.UseWhen(
+                context => context.Request.Path.StartsWithSegments(new PathString("/foo")),
+                a => a.Use(async (context, next) =>
+                {
+                    Console.WriteLine("B (before)");
+                    await next();
+                    Console.WriteLine("B (after)");
+                }));
+
+            app.Run(async context =>
+            {
+                Console.WriteLine("C");
+                await context.Response.WriteAsync("Hello world");
+            });
+        }
+    }
+}
