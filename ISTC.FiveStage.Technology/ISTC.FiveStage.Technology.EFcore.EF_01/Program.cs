8d1048e32afa175d7dee9dae513007b9524688a0
vanhakobyan <vanhakobyan1996@gmail.com>
2019-03-19 18:38:06 +0400

    minor chnages

---
+++ ISTC.FiveStage.Technology/ISTC.FiveStage.Technology.EFcore.EF_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISTC.FiveStage.Technology.EFcore.EF_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
