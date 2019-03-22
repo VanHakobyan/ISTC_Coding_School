1e80ac7e4239cf765fd41035ac05aa7a43f479a5
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2019-03-22 20:51:38 +0400

    minor chnages

---
+++ ISTC.FiveStage.Technology/ISTC.FiveStage.Technology.Logging.Nlog/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace ISTC.FiveStage.Technology.Logging.Nlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = LogManager.GetCurrentClassLogger();
+            Logger logger2 = LogManager.GetLogger("ISTCLogs");
+
+            for (int i = 0; i < 10; i++)
+            {
+                logger.Info($"iteration {i}");
+                logger2.Info($"iteration {i}");
+            }
+
+
+        }
+    }
+}
