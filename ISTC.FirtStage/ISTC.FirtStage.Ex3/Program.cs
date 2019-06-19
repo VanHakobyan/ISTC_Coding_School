c2e7e2e2e619166cce77d1b704511c69c561af58
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2019-06-19 20:59:13 +0400

    classwork

---
+++ ISTC.FirtStage/ISTC.FirtStage.Ex3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ISTC.FirtStage.Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var readLine = Console.ReadLine();
+                int n = int.Parse(readLine);
+                ulong factorial = 1;
+                for (uint i = 2; i < n + 1; i++)
+                {
+                    factorial *= i;
+                }
+                Console.WriteLine(factorial);
+            }
+            //factorial=1
+            //1. factorial = 2
+            //2. factorial=6
+            //3. 24
+            //4. 120
+            //5. 720
+           
+        }
+    }
+}
---
+++ ISTC.FirtStage/ISTC.FirtStage.Loop.GoTo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ISTC.FirtStage.Loop.GoTo
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = 123456;
+            int sum = 0;
+
+        L:
+            int digit = number % 10;//get last digit //23658585
+            sum += digit;
+            number = number / 10;   //cut last digit 
+
+            if (number < int.MaxValue)
+            {
+                goto L;
+            }
+
+            sum += number;
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+        }
+    }
+}
