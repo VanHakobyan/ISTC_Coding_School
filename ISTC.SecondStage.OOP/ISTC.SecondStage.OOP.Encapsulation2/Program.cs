b500ebb354330477d19023710800d922edb509f8
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2019-07-03 21:06:15 +0400

    lesson 2

--- ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.Encapsulation2/Program.cs
+++ ISTC.SecondStage.OOP/ISTC.SecondStage.OOP.Encapsulation2/Program.cs
@@ -6,45 +6,17 @@ using System.Threading.Tasks;
 
 namespace Encapsulation2
 {
-    public class Mard
-    {
-        public Mard(string name, int age, int qash)
-        {
-            anun = name;
-            tariq = age;
-            Qash = qash;
-        }
-
-        private int Qash;
-        private string anun;
-        public int tariq;
-        public void SayNameAndAge()
-        {
-            Console.WriteLine($"Es {anun}-n em {tariq} tarekan  !!!");
-        }
-        public void Method(string s)
-        {
-            anun = s;
-        }
-
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Mard mard1 = new Mard("John", 45, 451);
-            Mard mard2 = new Mard("Karen", 32, 78);
-            mard1.Method("new name");
-
-            Console.WriteLine(mard1.tariq);
-            mard1.tariq = 567;
-
-            Console.WriteLine(mard1.tariq);
-            Console.WriteLine(mard2.tariq);
-
-            mard1.SayNameAndAge();
-            mard2.SayNameAndAge();
+            var x = 15;
+            var str = string.Empty;
+            var str1 = "";
+            var f = 15f;
+            
+            //var an = new { Number = "", Lesson = "", Hour = 1};
+            
         }
     }
 }
