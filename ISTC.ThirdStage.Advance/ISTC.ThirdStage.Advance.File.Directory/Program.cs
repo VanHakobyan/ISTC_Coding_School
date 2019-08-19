fac002082b765d7c229077f2d9bb30ed4be92664
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2019-08-19 20:13:18 +0400

    file changes

--- ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.File.Directory/Program.cs
+++ ISTC.ThirdStage.Advance/ISTC.ThirdStage.Advance.File.Directory/Program.cs
@@ -11,6 +11,10 @@ namespace ISTC.ThirdStage.Advance.File.Directory
     {
         static void Main(string[] args)
         {
+            var currentDirectory = Environment.MachineName;
+            var userName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+
             DirectoryInfo dirinfo = new DirectoryInfo(@"C:\\Windows");
             Console.WriteLine($"Full Name of the directory is : {dirinfo.FullName}");
             Console.WriteLine($"The directory was last accesses on: { dirinfo.LastAccessTime}");
