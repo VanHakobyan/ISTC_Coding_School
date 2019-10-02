af39adc4274213f7b1235ff2862a93a9ec8ecabd
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2019-10-02 20:59:49 +0400

    DB first

---
+++ ISTC.FourthStage.Database/ISTC.FourthStage.Database.EF.DatabaseFirst2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.FourthStage.Database.EF.DatabaseFirst2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (MonitoringEntities db = new MonitoringEntities())
+            {
+                var profileAllInfoById = db.fn_GetLinkedinProfileAllInfoById(9).Where(x => x.ConnectionCount.HasValue).ToList();
+                //select * from linkedinProfile lp
+                //join linkedinLanguages ll on ll.LinkedinProfileId=lp.id
+                //db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ISTC.FourthStage.Database/ISTC.FourthStage.Database.EF.DatabaseFirst2/ProxyViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ISTC.FourthStage.Database.EF.DatabaseFirst2
+{
+    public class ProxyViewModel
+    {
+        public Guid Guid { get; set; }
+        public string Country { get; set; }
+        public string Ip { get; set; }
+        public string Port { get; set; }
+        public string Type { get; set; }
+        public int? Test { get; set; }
+    }
+}
